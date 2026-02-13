025ca7afe30b07dcb5db8a56ae076611386990fa
je1113 <jje320594@gmail.com>
2026-02-13 15:12:12 +0900

    모듈 인식시키기

--- netflix/settings.gradle.kts
+++ netflix/settings.gradle.kts
@@ -1,1 +1,14 @@
 rootProject.name = "netflix"
+
+include("netflix-adapters:adapter-http")
+include("netflix-adapters:adapter-persistence")
+
+include("netflix-apps:app-api")
+include("netflix-apps:app-batch")
+
+include("netflix-commons")
+
+include("netflix-core:core-domain")
+include("netflix-core:core-port")
+include("netflix-core:core-service")
+include("netflix-core:core-usecase")
