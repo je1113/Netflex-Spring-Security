038c6d2797d4e6c6b8e263f3ddfe051d11d9903c
je1113 <jje320594@gmail.com>
2026-02-13 16:00:31 +0900

    app-api springBootApp 적용

---
+++ netflix/netflix-apps/app-api/src/main/java/fast/campus/netflix/NetflixApiAppication.java
@@ -0,0 +1,11 @@
+package fast.campus.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixApiAppication {
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixApiAppication.class);
+    }
+}
