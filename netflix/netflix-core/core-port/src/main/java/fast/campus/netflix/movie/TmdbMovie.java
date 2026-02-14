2d0c4b63aa57ca6c9941aa385aa8ff7553c80cc2
je1113 <jje320594@gmail.com>
2026-02-14 11:24:48 +0900

    tmdb genre가 List객체라서 수정

--- netflix/netflix-core/core-port/src/main/java/fast/campus/netflix/movie/TmdbMovie.java
+++ netflix/netflix-core/core-port/src/main/java/fast/campus/netflix/movie/TmdbMovie.java
@@ -2,15 +2,17 @@ package fast.campus.netflix.movie;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class TmdbMovie {
     private final String movieName;
     private final Boolean isAdult;
-    private final String genre;
+    private final List<String> genre;
     private final String overview;
     private final String releaseAt;
 
-    public TmdbMovie(String movieName, Boolean isAdult, String genre, String overview, String releaseAt) {
+    public TmdbMovie(String movieName, Boolean isAdult, List<String> genre, String overview, String releaseAt) {
         this.movieName = movieName;
         this.isAdult = isAdult;
         this.genre = genre;
