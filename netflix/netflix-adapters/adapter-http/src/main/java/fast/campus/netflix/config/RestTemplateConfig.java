06dbbce7fd1f019c73368a4a8dcf1856562989a7
je1113 <jje320594@gmail.com>
2026-02-16 21:14:03 +0900

    kakao 소셜 로그인 http 작성

--- netflix/netflix-adapters/adapter-http/src/main/java/fast/campus/netflix/config/RestTemplateConfig.java
+++ netflix/netflix-adapters/adapter-http/src/main/java/fast/campus/netflix/config/RestTemplateConfig.java
@@ -5,12 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplateBuilder()
+                .setConnectTimeout(Duration.of(1, ChronoUnit.SECONDS))
+                .setReadTimeout(Duration.of(5, ChronoUnit.SECONDS))
                 .build();
     }
 }
