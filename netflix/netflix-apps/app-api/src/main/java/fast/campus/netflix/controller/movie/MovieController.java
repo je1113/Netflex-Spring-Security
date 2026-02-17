95ee90bcca816c275d3549cbdf9450bb2bcaeb36
je1113 <jje320594@gmail.com>
2026-02-17 11:44:57 +0900

    movie conrtoller 구현

--- netflix/netflix-apps/app-api/src/main/java/fast/campus/netflix/controller/movie/MovieController.java
+++ netflix/netflix-apps/app-api/src/main/java/fast/campus/netflix/controller/movie/MovieController.java
@@ -1,19 +1,22 @@
 package fast.campus.netflix.controller.movie;
 
+import fast.campus.netflix.controller.NetflixApiResponse;
 import fast.campus.netflix.movie.FetchMovieUseCase;
+import fast.campus.netflix.movie.response.PageableMoviesResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/movie")
 @RequiredArgsConstructor
 public class MovieController {
     private final FetchMovieUseCase fetchMovieUseCase;
 
-    @GetMapping("/api/v1/movie/client/{page}")
-    public String fetchMoviePageables(@PathVariable int page){
-        fetchMovieUseCase.fetchFromClient(page);
-        return "";
+    @PostMapping("/search")
+    @PreAuthorize("hasAnyRole('ROLE_FREE', 'ROLE_BRONZE', 'ROLE_SILVER', 'ROLE_GOLD')")
+    public NetflixApiResponse<PageableMoviesResponse> fetchMoviePageables(@PathVariable int page){
+        PageableMoviesResponse pageableMoviesResponse = fetchMovieUseCase.fetchFromClient(page);
+        return NetflixApiResponse.ok(pageableMoviesResponse);
     }
 }
